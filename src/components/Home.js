70eb52b4f58368f2a9e9b51db0a6408310088b4d
Jason <j.mortfield44@gmail.com>
2019-03-07 17:05:18 -0500

    Added more component files; added markup to home.js; added some assets

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,10 +6,19 @@ console.log(randomName());
 class Home extends Component {
     render() {
         return (
-            <div className="testing">
-                <h1>This is the home</h1>
-                <p>Testing testing</p>
-            </div>
+            <header className="home">
+                <h1 className="home__header">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, corporis.</h1>
+                <div className="home__logIn">
+                    <div className="home__logIn--border">
+                        <button className="home__logInButton">
+                            Log in
+                        </button>
+                        <button className="home__logInButton--guest">
+                            Sign in as guest
+                        </button>
+                    </div>
+                </div>
+            </header>
         )
     }
 }
